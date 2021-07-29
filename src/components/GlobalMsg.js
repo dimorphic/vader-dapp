c2ec43abda3ce3584277ba8591952c70320d1249
hankxstudio <hankx@momane.com>
2021-07-29 08:51:25 +0800

    WIP:add modal

---
+++ src/components/GlobalMsg.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Modal } from '@chakra-ui/react'
+
+const GlobalMsg = ()=>{
+	return ()=>{
+		<Modal>
+			<div>This is a modal</div>
+		</Modal>
+
+	}
+}
+
+
+export default GlobalMsg
